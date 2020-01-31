076b550110a23be29546e9dbe45e4d99299978f9
abhianshu2020 <abhianshu2020@gmail.com>
2020-01-31 16:52:01 +0530

    logout implemented

--- src/app/pages/e-commerce/e-commerce.component.ts
+++ src/app/pages/e-commerce/e-commerce.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-ecommerce',
   templateUrl: './e-commerce.component.html',
 })
 export class ECommerceComponent {
+  constructor(private router: Router){
+    const isUserLoggedIn = JSON.parse(localStorage.getItem('isUserLoggedIn'));
+
+    if(!isUserLoggedIn){
+      router.navigateByUrl('/login');
+    }
+  }
 }
--- src/app/services/auth/auth-service.service.ts
+++ src/app/services/auth/auth-service.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthServiceService {
 
   authURL = 'http://ec2-52-201-229-245.compute-1.amazonaws.com:3000/api/User/';
 
-  constructor(private httpClient: HttpClient, private apiService: ApiService) { }
+  constructor(private httpClient: HttpClient, private apiService: ApiService, private router: Router) { }
 
   userData: any;
 
@@ -19,6 +20,13 @@ export class AuthServiceService {
 
   register(userData){
     return this.apiService.post('http://ec2-52-201-229-245.compute-1.amazonaws.com:3000/api/User/', userData, null);
+  }
+
+  logout(){
+    localStorage.setItem('isUserLoggedIn', 'false');
+    this.router.navigateByUrl('/login');
+    return true;
+
   }
 
   checkIfLoggedIn(userId){

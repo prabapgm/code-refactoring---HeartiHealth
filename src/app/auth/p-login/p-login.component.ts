b003b44f04b12059d66fdac782b8ed0451ed8975
sanjayme97 <45826938+sanjayme97@users.noreply.github.com>
2020-03-12 15:04:44 +0530

    login and favicon

--- src/app/auth/p-login/p-login.component.ts
+++ src/app/auth/p-login/p-login.component.ts
@@ -19,14 +19,16 @@ export class PLoginComponent implements OnInit {
     private authService: AuthServiceService, private router: Router,
     private toastrService: NbToastrService) {
     this.checkoutForm = this.formBuilder.group({
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
   }
 
   onSubmit() {
     this.submitted = true;
-
+    if (this.checkoutForm.invalid){
+      return;
+    }
     this.authService.login(this.checkoutForm.value).subscribe((response: any) => {
       if (response.password === this.checkoutForm.value.password) {
         this.router.navigateByUrl('/pages');
@@ -34,8 +36,7 @@ export class PLoginComponent implements OnInit {
       }else{
         this.toastrService.show('Please enter correct username or password', 'Error!', { status: 'danger' });
       }
-    });
-    
+    }, error => { this.loginError(error); } );
 
     // stop here if form is invalid
     // if (this.checkoutForm.invalid) {
@@ -43,6 +44,18 @@ export class PLoginComponent implements OnInit {
     // }
   }
 
+  loginError(error) {
+   switch (error.error.error.code) {
+     case 'MODEL_NOT_FOUND':
+      this.toastrService.show('Please enter correct username or password', 'Error!', { status: 'danger' });
+       break;
+     default:
+      this.toastrService.show('Some Error occurred please try after sometime', 'Error!', { status: 'danger' });
+       break;
+   }
+  }
+  get email() { return this.checkoutForm.get('email'); }
+  get password() { return this.checkoutForm.get('password'); }
   ngOnInit() {
   }
 

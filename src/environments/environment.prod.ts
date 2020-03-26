a3a2c834f869855b3415ac1966e8f47718e69b69
Sharath-7 <62046509+Sharath-7@users.noreply.github.com>
2020-03-26 14:14:22 +0530

    Update environment.prod.ts
    Updated to http://172.30.11.153:8083/

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,9 @@
  */
 export const environment = {
   production: false,
-   heartiAttackCounterUrl : 'http://172.30.12.171:8083/api/intervalReport/weeklyReport/', 
+   heartiAttackCounterUrl : 'http://172.30.11.153:8083/api/intervalReport/weeklyReport/', 
  // totalPredictionUrl:'http://172.30.12.171:8085/api/',
-  totalPredictionUrl:'http://172.30.12.171:8083/api/getTotalPredictionByPeriod/',
-  livePredictionUrl :'http://172.30.12.171:8083/api/livePredictions/',
+  totalPredictionUrl:'http://172.30.11.153:8083/getTotalPredictionByPeriod/',
+  livePredictionUrl :'http://172.30.11.153:8083/api/livePredictions/',
 };
 
